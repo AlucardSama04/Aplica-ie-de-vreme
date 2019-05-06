27025991881662e6e68553590fe563b1c829c923
AlucardSama04 <42693393+AlucardSama04@users.noreply.github.com>
2019-05-06 21:25:39 +0300

    Update vremea.py
    Dacă orașul nu există, nu se va afișa nicio eroare a api-ului ci doar un mesaj. La fel și dacă orașul a fost scris greșit.

--- vremea.py
+++ vremea.py
@@ -1,30 +1,22 @@
 import requests
-from pprint import pprint
 
-oras = input('Introdu orasul : ')
-
-kelvin = '272.15'
-
-api = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=28cfde573f3c289f2551633d218f4e79'.format(oras)
-
+oras = input("Introdu Orașul: ")
+api = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=28cfde573f3c289f2551633d218f4e79&units=metric'.format(oras)
 info = requests.get(api)
-
 date = info.json()
 
-temp = date['main']['temp']
-viteza_vantului = date['wind']['speed']
+try:
+    temp = date['main']['temp']
+    vitezaVantului = date['wind']['speed']
+    latitudine = date['coord']['lat']
+    longitudine = date['coord']['lon']
+    descriere = date['weather'][0]['description']
 
-latitudine = date['coord']['lat']
-longitudine = date['coord']['lon']
-
-descriere = date['weather'][0]['description']
-
-#print('Temperatura: ', temp)
-print('Temperatura: ', float(temp)-float(kelvin), 'grade celsius')
-print('Viteza vantului: , {}' .format(viteza_vantului))
-print('Latitudine: {}' .format(latitudine))
-print('Longitudine: {}' .format(longitudine))
-print('Descriere: {}' .format(descriere))
-
-
-#print(float(temp)-float(kelvin))
+    print('Temperatura:', int(temp), 'grade celsius')
+    print('Viteza vantului: {}' .format(vitezaVantului))
+    print('Latitudine: {}' .format(latitudine))
+    print('Longitudine: {}' .format(longitudine))
+    print('Descriere: {}' .format(descriere))
+    
+except:
+    print("Oras inexistent sau scris gresit")
