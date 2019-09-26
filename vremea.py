e34549ca9a7a8130654b752f0eb4adb22d0b33ce
AlucardSama04 <42693393+AlucardSama04@users.noreply.github.com>
2019-09-26 20:44:36 +0300

    Update vremea.py
    Added args

--- vremea.py
+++ vremea.py
@@ -1,24 +1,35 @@
 import requests
+import argparse
 
-oras = input("Introdu Orașul: ")
-api = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=28cfde573f3c289f2551633d218f4e79&units=metric'.format(oras)
-info = requests.get(api)
-date = info.json()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--oras', required=True, help="Seteaza orasul care sa fie cautat.")
+    return parser.parse_args()
 
-try:
-    temp = date['main']['temp']
-    vitezaVantului = date['wind']['speed']
-    latitudine = date['coord']['lat']
-    longitudine = date['coord']['lon']
-    descriere = date['weather'][0]['description']
-    country = date['sys']['country']
+
+def main():
+    #Nu inteleg nimic din ce am scris pe aici
+    args = parse_args()
+    api = f'http://api.openweathermap.org/data/2.5/weather?q={args.oras}&appid=28cfde573f3c289f2551633d218f4e79&units=metric'
+    info = requests.get(api)
+    date = info.json()
+    try:
+        temp = date['main']['temp']
+        vitezaVantului = date['wind']['speed']
+        latitudine = date['coord']['lat']
+        longitudine = date['coord']['lon']
+        descriere = date['weather'][0]['description']
+        country = date['sys']['country']
+
+        print('Temperatura:', int(temp), 'grade celsius')
+        print('Viteza vantului: {}' .format(vitezaVantului))
+        print('Latitudine: {}' .format(latitudine))
+        print('Longitudine: {}' .format(longitudine))
+        print('Descriere: {}' .format(descriere))
+        print('Tara de origine: {}' .format(country))
+    except:
+        print("Oras scris incorect sau inexistent")
     
-    print('Temperatura:', int(temp), 'grade celsius')
-    print('Viteza vantului: {}' .format(vitezaVantului))
-    print('Latitudine: {}' .format(latitudine))
-    print('Longitudine: {}' .format(longitudine))
-    print('Descriere: {}' .format(descriere))
-    print('Tara de origine: {}' .format(country))
-    
-except:
-    print("Oras inexistent sau scris gresit")
+
+if __name__ == '__main__':
+    main()
